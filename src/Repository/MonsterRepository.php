6de6398503677ef5824dfba2517333f82ece7eee
joris <jorismartin3@gmail.com>
2025-10-07 21:46:00 +0200

    Fix doctrine error

--- src/Repository/MonsterRepository.php
+++ src/Repository/MonsterRepository.php
@@ -15,20 +15,4 @@ class MonsterRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Monster::class);
     }
-
-
-    /**
-     * @return Monster[]
-     */
-    public function findByGame(string $game, string $rank): array
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.games LIKE :gamePattern')
-            ->andWhere('m.ranks LIKE :rankPattern')
-            ->setParameter('gamePattern', '%'.$game.'%')
-            ->setParameter('rankPattern', '%'.$rank.'%')
-            ->getQuery()
-            ->getResult()
-        ;
-    }
 }
