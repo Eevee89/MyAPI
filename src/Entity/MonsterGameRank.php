f394c2db6709229085138210461b012216b11ade
joris <jorismartin3@gmail.com>
2025-10-07 21:43:47 +0200

    Fix doctrine error

--- src/Entity/MonsterGameRank.php
+++ src/Entity/MonsterGameRank.php
@@ -8,6 +8,11 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: MonsterGameRankRepository::class)]
 class MonsterGameRank
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+    
     #[ORM\Column(nullable: false)]
     private Monster $monster;
 
